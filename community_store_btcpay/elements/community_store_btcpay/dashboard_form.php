ac09419e5f298feef2a8963924ebee60e9731434
cahueya <cahueya@users.noreply.github.com>
2025-01-21 17:05:13 +0000

    Update dashboard_form.php
    Remove discount and payment method options. Unnecessary here.

--- community_store_btcpay/elements/community_store_btcpay/dashboard_form.php
+++ community_store_btcpay/elements/community_store_btcpay/dashboard_form.php
@@ -6,10 +6,7 @@ extract($vars);
     <?= $form->label('btcpayCurrency',t("Currency")); ?>
     <?= $form->select('btcpayCurrency',$currencies,$btcpayCurrency?$btcpayCurrency:'USD');?>
 </div>
-<div class="form-group">
-    <label><?= t("Discount")?><?= " " ?><?= t("in Percent")?></label>
-    <input type="number" name="btcpayDiscount" value="<?= $btcpayDiscount?>" class="form-control">
-</div>
+
 <div class="form-group">
     <label><?= t("BTC Payserver URL")?></label>
     <input type="url" name="btcpayUrl" value="<?= $btcpayUrl?>" class="form-control">
@@ -27,11 +24,6 @@ extract($vars);
 <div class="form-group">
     <label><?= t("BTC Payserver Webhook Secret")?></label>
     <input type="text" name="btcpayWebhooksecret" value="<?= $btcpayWebhooksecret?>" class="form-control">
-</div>
-
-<div class="form-group">
-    <?= $form->label('btcpayMethod',t("Payment Methods")); ?>
-    <?= $form->select('btcpayMethod',$paymethods,$btcpayMethod?$btcpayMethod:'BTC');?>
 </div>
 
 <div class="form-group">
